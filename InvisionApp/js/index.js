693f39b940c0c0b9da5775607116a9d1044d951b
CowboyKenbot <hey@thebeardedgent.com>
2015-08-26 19:52:58 -0400

    js edits

--- InvisionApp/js/index.js
+++ InvisionApp/js/index.js
@@ -1,8 +1,6 @@
-<script>
-  $(function() {
-    $('.expand').click(function(e) {
-      e.preventDefault();
-      $('.post-expanded').slideToggle(500);
-    });
+$(function() {
+  $('.expand').click(function(e) {
+    e.preventDefault();
+    $(this).parent().parent().parent().find('.post-expanded').animate({marginTop : "-35px"}, 50).slideToggle(500);
   });
-</script>
+});
